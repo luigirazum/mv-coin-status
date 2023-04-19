cea3556f393b0bdb983fd580e3c17dc921feb357
Luis Zubia <soyzubia@gmail.com>
2023-04-18 23:00:43 -0700

    Refactor AllCoins component renders a list of Coin(s) component

--- src/react/components/AllCoins.jsx
+++ src/react/components/AllCoins.jsx
@@ -1,20 +1,14 @@
-import { useSelector } from 'react-redux';
+import { useSelector, shallowEqual } from 'react-redux';
+import Coin from './Coin';
 
 const AllCoins = () => {
-  const coins = useSelector((store) => store.coins.assets);
+  const coins = useSelector((store) => store.coins.assets, shallowEqual);
 
   return (
-    <section>
-      <h2>All Coins</h2>
-      <ul>
-        {coins.map((coin) => (
-          <li key={coin.id}>
-            {coin.name}
-            <p>{coin.rank}</p>
-            <p>{coin.symbol}</p>
-          </li>
-        ))}
-      </ul>
+    <section className="allCoins">
+      {coins.map((coin) => (
+        <Coin key={coin.id} id={coin.id} />
+      ))}
     </section>
   );
 };
