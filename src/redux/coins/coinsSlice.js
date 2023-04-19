6ec5ac80aaba8d31d27848db1c61b266261476f2
Luis Zubia <soyzubia@gmail.com>
2023-04-18 17:23:54 -0700

    Create coinsSlice with lifecycle reducers for coins

---
+++ src/redux/coins/coinsSlice.js
@@ -0,0 +1,53 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchCoins } from './coinsActions';
+
+/** --------------------------------------------------
+ *   > Set initialState for coins
+ *  -------------------------------------------------- */
+const initialState = {
+  assets: [],
+  status: { type: 'idle' },
+  error: null,
+};
+
+const coinsSlice = createSlice({
+  name: 'coins',
+  initialState,
+  extraReducers: (builder) => builder
+  /** ------------------------------------------
+   *   > when we launch the request
+   *  ------------------------------------------ */
+    .addCase(fetchCoins.pending,
+      (state) => (
+        {
+          ...state,
+          status: { type: 'loading' },
+          error: null,
+        }
+      ))
+  /** ------------------------------------------
+   *   > if the coins are retrieved
+   *  ------------------------------------------ */
+    .addCase(fetchCoins.fulfilled,
+      (state, { payload }) => (
+        {
+          ...state,
+          assets: payload,
+          status: { type: 'idle' },
+          error: null,
+        }
+      ))
+  /** ------------------------------------------
+   *   > if something happens
+   *  ------------------------------------------ */
+    .addCase(fetchCoins.rejected,
+      (state, { payload }) => (
+        {
+          ...state,
+          status: { type: 'idle' },
+          error: payload,
+        }
+      )),
+});
+
+export default coinsSlice.reducer;
