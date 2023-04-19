403ae1eeecc7635e7f3140fcd8b231318b902702
Luis Zubia <soyzubia@gmail.com>
2023-04-18 17:59:41 -0700

    Import BrowserRouter at main App component && Routes for root, /coins and, /coins/:coin have been configured

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import { useDispatch } from 'react-redux';
 import '../styles/App.css';
 import { useEffect } from 'react';
+import {
+  BrowserRouter, Routes, Route, Outlet,
+} from 'react-router-dom';
 import { fetchCoins } from '../redux/coins/coinsActions';
 
 const App = () => {
@@ -11,14 +14,27 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <>
-      <header>
-        Main Nav
-      </header>
-      <main>
-        Start Menu
-      </main>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<h1>HOME</h1>} />
+        <Route
+          path="coins"
+          caseSensitive
+          element={
+            (
+              <>
+                <h3>NAVIGATION AT COINS</h3>
+                <Outlet />
+              </>
+)
+            }
+        >
+          <Route index caseSensitive element={<h3>LIST OF COINS</h3>} />
+          <Route path=":coin" caseSensitive element={<h4>DETAILS FOR A COIN</h4>} />
+        </Route>
+        <Route path="*" element={<h3>ERROR</h3>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
