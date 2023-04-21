098e28b8ba29ee20120d59960cf5cf2ae4ea5587
Luis Zubia <soyzubia@gmail.com>
2023-04-21 13:52:45 -0700

    Import detailsReducer into the store to handle the state for showing details feature

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import coinsReducer from './coins/coinsSlice';
+import detailsReducer from './details/detailsSlice';
 
 const rootReducer = combineReducers({
   coins: coinsReducer,
+  details: detailsReducer,
 });
 
 const setupStore = (
