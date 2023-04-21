5cdb182ba3d5a679b0ad899598d815c539df14a2
Luis Zubia <soyzubia@gmail.com>
2023-04-21 13:48:03 -0700

    Import selector to render a single coin && Add classes to Coin elements for styling purposes

--- src/react/components/Coin.jsx
+++ src/react/components/Coin.jsx
@@ -1,26 +1,31 @@
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { selectCoinById } from '../../redux/coins/coinsSelectors';
 
 const Coin = ({ id }) => {
-  const [coin] = useSelector((store) => store.coins.assets.filter((coin) => coin.id === id));
+  const coin = useSelector((store) => selectCoinById(store, id));
   const {
     icon, name, symbol, rank,
   } = coin;
 
   return (
     <article className="coinItem">
-      <div className="iconContainer">
-        <img src={icon} className="coinIcon" alt={name} />
-      </div>
-      <h3>{name}</h3>
-      <p>
-        Symbol
-        <span>{symbol}</span>
-      </p>
-      <p>
-        Rank
-        <span>{rank}</span>
-      </p>
+      <section className="itemSection itemHeader">
+        <h3 className="itemName">{name}</h3>
+        <div className="iconContainer">
+          <img src={icon} className="coinIcon" alt={name} />
+        </div>
+      </section>
+      <section className="itemSection">
+        <div className="itemDescription">
+          <h3 className="itemCategory rankCategory">Rank</h3>
+          <p className="itemData rankData">{rank}</p>
+        </div>
+        <div className="itemDescription">
+          <h4 className="itemCategory">Symbol</h4>
+          <p className="itemData">{symbol}</p>
+        </div>
+      </section>
     </article>
   );
 };
