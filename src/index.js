be0b6a2c34c675e88b13ae1402ddddd01fe41513
Luis Zubia <soyzubia@gmail.com>
2023-04-26 19:02:18 -0700

    Add conditional mocking API with a env.local variable

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import setupStore from './redux/store';
 import App from './react/App';
+import worker from './tests/mocks/msw/browser';
+
+if (process.env.REACT_APP_API === 'intercept') {
+  worker.start();
+} else {
+  worker.stop();
+}
 
 const rootTarget = document.getElementById('root');
 const root = ReactDOM.createRoot(rootTarget);
