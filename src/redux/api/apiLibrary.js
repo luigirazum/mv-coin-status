e32e356e4130d9c48a980ca7e90dce2dfdb98878
Luis Zubia <soyzubia@gmail.com>
2023-04-27 13:14:35 -0700

    Add conditional return for request api url generators

--- src/redux/api/apiLibrary.js
+++ src/redux/api/apiLibrary.js
@@ -3,28 +3,28 @@ import axios from 'axios';
 /** ----------------------------------------------------------------------
  *   > API endpoints Url generators
  *  ---------------------------------------------------------------------- */
-export const allCoinsUrl = ({ mockVersion = null } = {}) => {
+export const allCoinsUrl = (mockedAPI = null) => {
   const baseAllCoinsUrl = 'https://api.coinstats.app/public/v1/coins';
 
-  if (mockVersion) return baseAllCoinsUrl;
-
-  return `${baseAllCoinsUrl}?skip=0&currency=USD`;
+  return ((process.env.REACT_APP_API === 'intercept') && mockedAPI)
+    ? baseAllCoinsUrl
+    : `${baseAllCoinsUrl}?skip=0&currency=USD`;
 };
 
-export const coinHistoryUrlById = (coinId, { mockVersion = null } = {}) => {
+export const coinHistoryUrlById = (coinId, mockedAPI = null) => {
   const baseCoinHistoryUrlById = 'https://api.coinstats.app/public/v1/charts';
 
-  if (mockVersion) return baseCoinHistoryUrlById;
-
-  return `${baseCoinHistoryUrlById}?period=1w&coinId=${coinId}`;
+  return ((process.env.REACT_APP_API === 'intercept') && mockedAPI)
+    ? baseCoinHistoryUrlById
+    : `${baseCoinHistoryUrlById}?period=1w&coinId=${coinId}`;
 };
 
-export const coinMarketsUrlById = (coinId, { mockVersion = null } = {}) => {
+export const coinMarketsUrlById = (coinId, mockedAPI = null) => {
   const baseCoinMarketsUrlById = 'https://api.coinstats.app/public/v1/markets';
 
-  if (mockVersion) return baseCoinMarketsUrlById;
-
-  return `${baseCoinMarketsUrlById}?coinId=${coinId}`;
+  return ((process.env.REACT_APP_API === 'intercept') && mockedAPI)
+    ? baseCoinMarketsUrlById
+    : `${baseCoinMarketsUrlById}?coinId=${coinId}`;
 };
 
 /** ----------------------------------------------------------------------
