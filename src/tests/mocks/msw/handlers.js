7ff4f8b1694b8e8a004f46023b1a42281ea01ec8
Luis Zubia <soyzubia@gmail.com>
2023-04-26 19:01:23 -0700

    Add handler to manage the mock API requests && Add server to be used at tests

--- src/tests/mocks/msw/handlers.js
+++ src/tests/mocks/msw/handlers.js
@@ -4,26 +4,26 @@ import { mockCoins } from '../mockData';
 
 console.log(allCoinsUrl({ mockVersion: true }));
 const handlers = [
-  rest.get(allCoinsUrl(), (req, res, ctx) => {
-    const { skip, currency } = req.searchParams;
-    console.log('skip', skip);
-    console.log('currency', currency);
+  rest.get(allCoinsUrl({ mockVersion: true }), (req, res, ctx) => {
+    const { searchParams } = req.url;
+    const { skip, currency } = Object
+      .fromEntries(searchParams.entries());
 
-    // if (skip === 0 && currency === 'USD') {
+    if (Number(skip) === 0 && currency === 'USD') {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          ...mockCoins,
+        }),
+      );
+    }
+
     return res(
-      ctx.status(200),
+      ctx.status(404),
       ctx.json({
-        ...mockCoins,
+        error: { message: 'not found' },
       }),
     );
-    // }
-
-    // return res(
-    //   ctx.status(404),
-    //   ctx.json({
-    //     error: { message: 'not found' },
-    //   }),
-    // );
   }),
 ];
 
