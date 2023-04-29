6c2ef2a7036bc0c910d03214c3dd5cfb6416ef48
Luis Zubia <soyzubia@gmail.com>
2023-04-28 22:50:23 -0700

    Refactor App just renders the Routes for the App

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -1,30 +1,19 @@
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { fetchCoins } from '../redux/coins/coinsActions';
 import Home from './routes/Home';
 import CoinsLayout from './routes/CoinsLayout';
 import AllCoins from './routes/AllCoins';
 import CoinDetails from './routes/CoinDetails';
 import '../styles/App.css';
 
-const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchCoins());
-  }, [dispatch]);
-
-  return (
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="coins" caseSensitive element={<CoinsLayout />}>
-        <Route index caseSensitive element={<AllCoins />} />
-        <Route path=":id" caseSensitive element={<CoinDetails />} />
-      </Route>
-      <Route path="*" element={<h3>ERROR</h3>} />
-    </Routes>
-  );
-};
+const App = () => (
+  <Routes>
+    <Route index element={<Home />} />
+    <Route path="coins" caseSensitive element={<CoinsLayout />}>
+      <Route index caseSensitive element={<AllCoins />} />
+      <Route path=":id" caseSensitive element={<CoinDetails />} />
+    </Route>
+    <Route path="*" element={<h3>ERROR</h3>} />
+  </Routes>
+);
 
 export default App;
