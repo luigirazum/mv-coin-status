f0b89bf899db835c0d77eae396a4aebe6d0ccedd
Luis Zubia <soyzubia@gmail.com>
2023-05-07 23:49:52 -0700

    Add url generators for retrieving svgs

--- src/redux/coins/coinsActions.js
+++ src/redux/coins/coinsActions.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { allCoinsUrl } from '../api/apiLibrary';
+import { allCoinsUrl, coinSvgIconMonoUrlBySymbol } from '../api/apiLibrary';
 
 /** -----------------------------------------------------
  *   > Action type definitions
@@ -14,9 +14,52 @@ const fetchCoins = createAsyncThunk(
   FETCH_COINS,
   async (_, thunkAPI) => {
     try {
-      const resp = await axios
-        .get(allCoinsUrl());
-      return resp.data;
+      const resp = await axios.get(allCoinsUrl());
+      const { data: { coins } } = resp;
+      const options = {
+        headers: { Accept: 'image/svg+xml' },
+      };
+      const coinsIconsMonoRequests = coins
+        .map(({ symbol }) => axios.get(coinSvgIconMonoUrlBySymbol(symbol), options));
+      const coinsIconsMono = await Promise.allSettled(coinsIconsMonoRequests);
+      const resultsSvgsMono = coinsIconsMono
+        .map((resp, index) => {
+          const { status } = resp;
+          if (status !== 'fulfilled') return null;
+
+          const {
+            value: {
+              request: {
+                responseXML,
+              },
+            },
+          } = resp;
+
+          const elem = responseXML.documentElement.cloneNode(true);
+          elem.removeAttribute('xmlns:xlink');
+          elem.removeAttribute('style');
+          elem.setAttribute('fill', 'currentColor');
+          elem.setAttribute('id', `${coins[index].id}-icon-${coins[index].symbol.toLowerCase()}-svg-mono`);
+          const XMLS = new XMLSerializer();
+          const elemXMLS = XMLS.serializeToString(elem);
+          return elemXMLS;
+        });
+      const coinsSvgsMono = coins.map((coin, index) => {
+        const svg = resultsSvgsMono[index];
+
+        if (!svg) return coin;
+
+        return ({
+          ...coin,
+          svg,
+        });
+      });
+
+      return ({
+        ...resp.data,
+        coins: coinsSvgsMono,
+      });
+      // return resp.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
